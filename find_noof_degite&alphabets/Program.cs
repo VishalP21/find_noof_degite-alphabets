296e8a344efd1d959c94453164334c6423e224f0
Vishal Prajapati <vishalp951295@gmail.com>
2024-07-12 11:32:31 +0530

    Add project files.

---
+++ find_noof_degite&alphabets/Program.cs
@@ -0,0 +1,6 @@
+﻿using find_noof_degite_alphabets;
+
+alpha pin = new();
+pin.Input();
+pin.process();
+pin.output();
---
+++ find_noof_degite&alphabets/alpha.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_noof_degite_alphabets
+{
+    internal class alpha
+    {
+        string name = "";
+        int alphabets = 0;
+        int numbers = 0;
+        public void Input()
+        {
+            Console.WriteLine("Enter a name with some degite: ");
+            name = Console.ReadLine();
+        }
+        public void process()
+        {
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (name[i]>='A' &&  name[i]<='Z' ||  name[i]>='a' && name[i] <= 'z')
+                {
+                    alphabets++;
+                }
+                else if (name[i] >= '0' && name[i] <= '9')
+                {
+                    numbers++;
+                }
+            }
+
+        }
+        public void output()
+        {
+            Console.WriteLine("alphabets = "+alphabets);
+            Console.WriteLine("degite = "+numbers);
+        }
+    }
+}
